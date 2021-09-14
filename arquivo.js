f96c0cbae8e427666831c3fb0872f869a55583eb
GuilhermeVinicius20 <gui,vinicius79@gmail.com>
2021-09-14 16:16:28 -0300

    Curriculo site

--- arquivo.js
+++ arquivo.js
@@ -1,1 +1,16 @@
-alert('Fica tranquilebaa!')
+alert('Olá este é meu site curriculo espero que gostem :)')
+const radios1 = document.getElementById('box1')
+const radios2 = document.getElementById('box2')
+const texto = document.getElementById('texto1')
+
+function capturar1(){
+    if(radios1.checked)
+      alert('Obrigado pelo feedback')   
+}
+ 
+function capturar2(){
+    if(radios2.checked)
+    var resultado = window.prompt('O que pode melhorar?')
+    texto.innerHTML = (resultado)
+   
+}
